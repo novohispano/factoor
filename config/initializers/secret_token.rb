4f60293ea6f84b8ae9c895353c733fe44898aadd
novohispano@gmail.com <novohispano@gmail.com>
2013-08-02 11:27:47 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Factoor::Application.config.secret_key_base = 'aab28991a2da4c90b89bf882b4aee0b59a5366cebebd63df4cadd7f64b82fc5e19c8d007414c1f701f9280101557cb09f2d2a83b3b5c6103fabd05c337c664be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Factoor::Application.config.session_store :cookie_store, key: '_factoor_session'
